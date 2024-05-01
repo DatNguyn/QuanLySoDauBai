22ef1ee0ecaf0783d7278e95f292c10822f4930a
Mai Thi Thanh Loan <22520782@gm.uit.edu.vn>
2024-05-01 22:39:39 +0700

    sửa 1/5

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".menu-qldt");
+btn.addEventListener("click", (event) => {
+  let inputNode = document.querySelector(".submenu-qldt");
+  inputNode.style.display = inputNode.style.display === "block" ? "none" : "block";
+});
