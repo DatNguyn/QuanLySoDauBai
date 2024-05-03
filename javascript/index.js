82fbe3b90b6089cb822ceea8dd5eb27ac0d9a990
Nguyen Tien Dat <dattnguyen602@gmail.com>
2024-05-03 17:40:06 +0700

    Them trang tong quan

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector(".menu-qldt");
 btn.addEventListener("click", (event) => {
   let inputNode = document.querySelector(".submenu-qldt");
-  inputNode.style.display = inputNode.style.display === "block" ? "none" : "block";
+  inputNode.style.display =
+    inputNode.style.display === "block" ? "none" : "block";
 });
--- javascript/login.js
+++
@@ -1,9 +0,0 @@
-const passwordInput = document.getElementById("password");
-
-// Lắng nghe sự kiện khi giá trị trong ô nhập mật khẩu thay đổi
-passwordInput.addEventListener("input", function () {
-  // Giới hạn độ dài của mật khẩu hiển thị là 10 ký tự
-  const displayedPassword = this.value.substring(0, 10);
-  // Hiển thị mật khẩu đã giới hạn độ dài
-  this.value = displayedPassword;
-});
