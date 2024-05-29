8e144ebc53f85eca744125cde795a65dcbc65003
NgocLinh711 <22520761@gm.uit.edu.vn>
2024-05-29 16:35:41 +0700

    sua format

--- javascript/aside_nav.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".aside_nav-qldt");
-btn.addEventListener("click", (event) => {
-  let inputNode = document.querySelector(".subaside_nav-qldt");
-  inputNode.style.display =
-    inputNode.style.display === "block" ? "none" : "block";
-});
